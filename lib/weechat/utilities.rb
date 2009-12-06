e8daf71a556c35cf4f71d5a9d93c656ad25354eb
Dominik Honnef <dominikho@gmx.net>
2009-12-06 05:23:53 +0100

    rewrote Utilities.apply_transformation

--- lib/weechat/utilities.rb
+++ lib/weechat/utilities.rb
@@ -1,13 +1,10 @@
 module Weechat
   module Utilities
     def self.apply_transformation(property, value, transformations)
-      transformations.each do |properties, transformation|
-        if properties.include?(property.to_sym)
-          value = transformation.call(value)
-          break
-        end
-      end
-      value
+      transformation = transformations.find {|properties, transformation|
+        properties.include?(property.to_sym)
+      }[1]
+      transformation.call(value)
     end
   end
 end

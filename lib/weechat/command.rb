fec238ef271e22728b4040668a418611ec93229f
Dominik Honnef <dominikho@gmx.net>
2009-12-08 18:19:58 +0100

    if a new command starts with a slash, cut it off

--- lib/weechat/command.rb
+++ lib/weechat/command.rb
@@ -7,13 +7,15 @@ module Weechat
     attr_reader :completion
     def initialize(command, description, args=nil, args_description=nil, completion=nil, &callback)
       super
+      command = command.to_s
+      command[0..0] = '' if command[0..0] == '/'
       @command          = command
       @description      = description
       @args             = args
       @args_description = args_description
       @completion       = completion
       @callback         = callback
-      @ptr              = Weechat.hook_command(command.to_s,
+      @ptr              = Weechat.hook_command(command,
                                        description.to_s,
                                        args.to_s,
                                        args_description.to_s,

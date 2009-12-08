4fd059a34530f41c814777c1883164f54f391809
Dominik Honnef <dominikho@gmx.net>
2009-12-08 09:30:08 +0100

    implemented log method

--- lib/weechat.rb
+++ lib/weechat.rb
@@ -51,6 +51,13 @@ module Weechat
     puts(object.pretty_inspect, buffer)
   end
 
+  # Writes text to the WeeChat log +weechat.log+
+  #
+  # @return [void]
+  def self.log(text)
+    Weechat.log_print(text)
+  end
+
   def self.integer_to_bool(int)
     int == 0 ? false : true
   end

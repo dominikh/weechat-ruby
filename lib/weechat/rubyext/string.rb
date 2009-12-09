1b7ee01dd28a5de576fd3fde293fce0b1ff62d04
Dominik Honnef <dominikho@gmx.net>
2009-12-09 01:22:05 +0100

    added shell like splitting for command arguments

--- lib/weechat/rubyext/string.rb
+++ lib/weechat/rubyext/string.rb
@@ -10,4 +10,65 @@ class String
   end
   alias_method :remove_colors!, :remove_color!
   alias_method :strip_colors!, :remove_color!
+
+  def shell_split
+    out = [""]
+    state = :none
+    escape_next = false
+    quote = ""
+    strip.split(//).each do |char|
+      case state
+      when :none, :space
+        case char
+        when /\s/
+          out << "" unless state == :space
+          state = :space
+          escape_next = false
+        when "\\"
+          if escape_next
+            out.last << char
+            escape_next = false
+          else
+            escape_next = true
+          end
+        when '"', "'"
+          if escape_next
+            out.last << char
+            escape_next = false
+          else
+            state = char
+            quote = ""
+          end
+        else
+          state = :none
+          out.last << char
+          escape_next = false
+        end
+      when '"', "'"
+        case char
+        when '"', "'"
+          if escape_next
+            quote << char
+            escape_next = false
+          elsif char == state
+            out.last << quote
+            state = :none
+          else
+            quote << char
+          end
+        when '\\'
+          if escape_next
+            quote << char
+            escape_next = false
+          else
+            escape_next = true
+          end
+        else
+          quote << char
+          escape_next = false
+        end
+      end
+    end
+    out
+  end
 end

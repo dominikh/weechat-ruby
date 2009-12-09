be6a5faface66dc052edf8b7e6cc2662b74620b5
Dominik Honnef <dominikho@gmx.net>
2009-12-09 21:56:21 +0100

    rewrote Line class

--- lib/weechat/line.rb
+++ lib/weechat/line.rb
@@ -8,26 +8,51 @@ module Weechat
   class Line
     class << self
       def parse(line)
-        new(*line.split("\t"))
+        parts = line.split("\t")
+        case parts.size
+        when 0
+          parts = ["", ""]
+        when 1
+          parts.unshift ""
+        end
+
+        new(*parts)
+      end
+
+      def from_hash(h)
+        prefix  = h.delete(:prefix)
+        message = h.delete(:message)
+        new(prefix, message, h)
       end
     end
 
+
+    %w(y date date_printed str_time tags_count tags displayed highlight last_read_line).each do |prop|
+      define_method(prop) { details[prop] }
+      define_method("#{prop}=") {|v| details[prop] = v }
+    end
+
     attr_accessor :prefix
-    attr_accessor :text
-    def initialize(prefix, text)
-      @prefix, @text = prefix, text
+    attr_accessor :message
+    attr_reader   :details
+    def initialize(prefix, message, details = {})
+      @prefix, @message, @details = prefix, message, details
     end
 
     def to_s
-      [@prefix, @text].join("\t")
+      join("\t")
+    end
+
+    def join(delimiter)
+      [prefix.empty? ? nil : prefix, message].compact.join(delimiter)
     end
 
     def method_missing(m, *args)
       rets = []
-      [@prefix, @text].each do |var|
+      [prefix, message].each do |var|
         rets << var.__send__(m, *args) rescue var
       end
-      rets.compact.join("\t")
+      [rets[0].empty? ? nil : rets[0], rets[1]].compact.join("\t")
     end
   end
 end

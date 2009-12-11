75cb4b8736b47fa72940f06db2e3ca7fdf12053c
Dominik Honnef <dominikho@gmx.net>
2009-12-11 10:07:07 +0100

    don't display warning when undefining object_id

--- lib/weechat/blankslate.rb
+++ lib/weechat/blankslate.rb
@@ -1,4 +1,13 @@
+class NullOutput
+  def write(*args)
+  end
+end
+
 class Blankslate
   alias_method :__class__, :class
+
+  old_stderr = $stderr.dup
+  $stderr = NullOutput.new
   instance_methods.each { |m| undef_method m unless m =~ /^__/ }
+  $stderr = old_stderr
 end

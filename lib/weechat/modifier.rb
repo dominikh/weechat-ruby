8f5fae98887702f3c5075092cf3190068776389e
Dominik Honnef <dominikho@gmx.net>
2009-12-13 06:36:11 +0100

    added support for directly calling modifier

--- lib/weechat/modifier.rb
+++ lib/weechat/modifier.rb
@@ -11,5 +11,14 @@ module Weechat
                                    # value of the callback
       @ptr              = Weechat.hook_modifier(modifier, "modifier_callback", id.to_s)
     end
+
+    alias_method :exec, :call
+
+    class << self
+      def call(modifier, data, string)
+        Weechat.hook_modifier_exec(modifier.to_s, data.to_s, string.to_s)
+      end
+      alias_method :exec, :call
+    end
   end
 end

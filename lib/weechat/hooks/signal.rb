105a6aa4a788f8090099930c6806a13b5b524bd2
Dominik Honnef <dominikho@gmx.net>
2009-12-24 13:07:07 +0100

    implemented weechat_hook_signal_send

--- lib/weechat/hooks/signal.rb
+++ lib/weechat/hooks/signal.rb
@@ -6,6 +6,13 @@ module Weechat
         @callback = EvaluatedCallback.new(callback)
         @ptr      = Weechat.hook_signal(signal, "signal_callback", id.to_s)
       end
+
+      class << self
+        def send(signal, type, data)
+          Weechat.hook_signal_send(signal.to_s, type.to_s, data.to_s)
+        end
+        alias_method :exec, :send
+      end
     end
   end
 end
